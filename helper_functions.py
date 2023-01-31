1ceee01002a2945507c9906690fa495c358ef249
Graham Woolley <gwoolley@berkeley.edu>
2023-01-31 07:41:43 -0800

    finished scraping sunroof for Traverse City and saved the results to a csv

--- helper_functions.py
+++ helper_functions.py
@@ -3,5 +3,14 @@ def get_numeric(string: str) -> int:
     Extracts only the numeric characters in a string and returns its integer version
     '''
     number = string.split(' ')[0]
-    return int(number.replace(',', ''))
-
+    if ',' in number: 
+        number = number.replace(',', '')
+    if '$' in number: 
+        number = number.replace('$', '')
+    if '.' in number: 
+        return float(number)
+    try: 
+        return int(number)
+    except ValueError: 
+        return string
+ 
